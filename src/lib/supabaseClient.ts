431cf7016b33e8802f574a5c46f9471831b47a96
uchi chinyama <67492491+uchi-c@users.noreply.github.com>
2026-06-17 18:13:22 +0200

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const metaEnv = (import.meta as any).env || {};
-const supabaseUrl = metaEnv.https://exovfiaalghyuslbhbnf.supabase.co || "";
+const supabaseUrl = metaEnv.sb_publishable_8Qvs7KDRFxLGs7PvbVGiOQ_b55_Ipn8||"";
 const supabaseAnonKey = metaEnv.sb_publishable_8Qvs7KDRFxLGs7PvbVGiOQ_b55_Ipn8 || "";
 
 export const supabase = (supabaseUrl && supabaseAnonKey)
